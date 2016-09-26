fa1c593818924ef2a284776d8ff314fde656f00a
Ivan Veselov <veselov.iva@gmail.com>
2016-09-26 22:08:25 +0300

    Add wrapper functions implementation.

--- src/io.c
+++ src/io.c
@@ -1,12 +1,28 @@
 #include <io.h>
 
 uint64_t printf(const char *format, ...) {
+  va_list arg;
+  va_start(arg, format);
+  
+  uint64_t result = vprintf(format, arg);
+  
+  va_end(arg);
+  
+  return result;
 }
 
 uint64_t vprintf(const char *format, va_list arg) {
 }
 
 uint64_t snprintf(char *s, uint64_t n, const char *format, ...) {
+  va_list arg;
+  va_start(arg, format);
+  
+  uint64_t result = vsnprintf(s, n, format, arg);
+  
+  va_end(arg);
+  
+  return result;
 }
 
 uint64_t vsnprintf(char *s, uint64_t n, const char *format, va_list arg) {
